145ed948e5da655e6cc16c420bee3225a6bec03c
Oleksii Romantsev <lexx.ra@gmail.com>
2025-10-13 12:23:37 +0300

    base init repo

---
+++ assets/script.js
@@ -0,0 +1,54 @@
+function LotteryCard(index, isWinning) {
+  this.index = index;
+  this.isWinning = isWinning;
+  
+}
+
+LotteryCard.prototype.handleTry = function (attempt, square, squareContainer) {
+  if (this.isWinning) {
+    alert("Congrats! You have won!");
+    square.classList.add("square-correct");
+    setTimeout(() => { squareContainer.innerHTML = null; return; },2000);
+  } else {
+    if (attempt === 3) {
+      alert("Sorry, it was your last turn");
+      squareContainer.innerHTML = null;
+      return;
+    }
+    alert("Sorry, try one more time!");
+    square.classList.add("square-wrong");
+  }
+};
+
+const cards = [
+  new LotteryCard(7, true),
+  new LotteryCard(1, false),
+  new LotteryCard(6, false),
+  new LotteryCard(10, false),
+  new LotteryCard(8, false),
+  new LotteryCard(2, false),
+  new LotteryCard(9, false),
+  new LotteryCard(11, false),
+  new LotteryCard(3, false),
+  new LotteryCard(5, false),
+  new LotteryCard(4, false),
+];
+
+function renderSquares(squares){
+  let attempt = 0;
+  const squareContainer = document.querySelector('.squares-container');
+  squares.forEach(item => {
+    const square = document.createElement('div');
+    const squareIndex = document.createElement('h4');
+    squareIndex.innerHTML = item.index;
+    square.appendChild(squareIndex);
+    square.classList.add('square');
+    square.addEventListener('click', () => {
+      item.handleTry(++attempt, square, squareContainer)
+    })
+    squareContainer.appendChild(square);
+  });
+};
+
+renderSquares(cards);
+
